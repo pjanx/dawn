acaa561d341ff914af0d71cb414b20b43bedd98d
Přemysl Eric Janouch <p@janouch.name>
2026-08-27 00:35:08 +0200

    Move hints into kit-chrome

--- dn/kit-chrome.hpp
+++ dn/kit-chrome.hpp
@@ -9,7 +9,6 @@
 
 #include "action.hpp"
 #include "kit.hpp"
-#include "kit-hint.hpp"
 
 #include <QUrl>
 
@@ -50,6 +49,46 @@ struct Sidebar : Panel {
 
 	explicit Sidebar(std::unique_ptr<Widget> child);
 	bool key(Kit &kit, int key, unsigned mods) override;
+};
+
+struct Browser;
+struct Page;
+
+struct Hint : Popup {
+	Page *page = nullptr;
+
+	Hint();
+	void open(Kit &kit);
+	void close(Kit &kit) override;
+	void place(Kit &kit) override;
+	void prepare(Kit &kit) override;
+	void paint(Kit &kit) const override;
+	bool captures_keys() const override { return true; }
+	bool key(Kit &kit, int key, unsigned mods) override;
+	bool press(Kit &kit, float x, float y, Qt::MouseButton button) override;
+	bool release(Kit &kit, float x, float y, Qt::MouseButton button) override;
+	bool motion(Kit &kit, float x, float y) override;
+
+private:
+	struct Target {
+		QString label;
+		Rect at{};
+		Rect chip{};
+		Button *button = nullptr;
+		// TODO(p): Unwanted dependency.
+		Browser *browser = nullptr;
+		int file_i = -1;
+	};
+
+	std::vector<Target> targets_;
+	QString typed_;
+
+	void collect();
+	void assign_labels();
+	void refresh_rects();
+	void layout_chips(const Kit &kit);
+	[[nodiscard]] bool matches(const Target &t) const;
+	void fire(Kit &kit, Target t);
 };
 
 struct Page : Composite {
